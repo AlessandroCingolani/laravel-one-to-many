550fb87ebbb81c437fddd4681ffa62081368de77
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-01 16:03:26 +0100

    [ init ] type-project to display type/relate proj

---
+++ resources/views/admin/types/type-project.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>List Type with related projects</h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Type</th>
+                <th scope="col">Projects related</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($types as $type)
+                <tr>
+                    <td>{{ $type->id }}</td>
+                    <td>{{ $type->name }}</td>
+                    <td>
+                        <ul class="list-group">
+                            @foreach ($type->projects as $project)
+                                <li class="list-group-item">
+                                    <a href="{{ route('admin.projects.show', $project) }}">{{ $project->name }}</a>
+
+                                </li>
+                            @endforeach
+                        </ul>
+                    </td>
+
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
