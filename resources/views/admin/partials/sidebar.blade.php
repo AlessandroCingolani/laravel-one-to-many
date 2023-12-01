1dcd9871313b7effce4c936789a6eb7780ce2cad
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-01 16:01:36 +0100

    [ add ] current route for active class scss

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -3,19 +3,29 @@
     <nav>
         <ul class="p-0">
             <li>
-                <a href="{{ route('admin.home') }}"><i class="fa-solid fa-chart-line"></i>Dashboard</a>
+                <a class="{{ Route::currentRouteName() === 'admin.home' ? 'active' : '' }}"
+                    href="{{ route('admin.home') }}"><i class="fa-solid fa-chart-line"></i>Dashboard</a>
             </li>
             <li>
-                <a href="{{ route('admin.projects.index') }}"><i class="fa-solid fa-list"></i>List projects</a>
+                <a class="{{ Route::currentRouteName() === 'admin.projects.index' ? 'active' : '' }}"
+                    href="{{ route('admin.projects.index') }}"><i class="fa-solid fa-list"></i>List projects</a>
             </li>
             <li>
-                <a href="{{ route('admin.projects.create') }}"><i class="fa-solid fa-folder-plus"></i>New project</a>
+                <a class="{{ Route::currentRouteName() === 'admin.projects.create' ? 'active' : '' }}"
+                    href="{{ route('admin.projects.create') }}"><i class="fa-solid fa-folder-plus"></i>New project</a>
             </li>
             <li>
-                <a href="{{ route('admin.technologies.index') }}"><i class="fa-solid fa-microchip"></i>Technologies</a>
+                <a class="{{ Route::currentRouteName() === 'admin.technologies.index' ? 'active' : '' }}"
+                    href="{{ route('admin.technologies.index') }}"><i class="fa-solid fa-microchip"></i>Technologies</a>
             </li>
             <li>
-                <a href="{{ route('admin.types.index') }}">Types</a>
+                <a class="{{ Route::currentRouteName() === 'admin.types.index' ? 'active' : '' }}"
+                    href="{{ route('admin.types.index') }}"><i class="fa-solid fa-window-restore"></i>Types</a>
+            </li>
+            <li>
+                <a class="{{ Route::currentRouteName() === 'admin.type-project' ? 'active' : '' }}"
+                    href="{{ route('admin.type-project') }}"><i class="fa-solid fa-layer-group"></i>Projects by
+                    type</a>
             </li>
         </ul>
     </nav>
