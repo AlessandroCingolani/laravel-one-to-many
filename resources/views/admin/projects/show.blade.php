34d4e59646069cd497b5b791f44fe6096e95fd4d
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-03 15:35:22 +0100

    [ add ] helper format date

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -10,12 +10,13 @@
     @if ($project->type)
         <p>Type name:<strong>{{ $project->type->name }}</strong></p>
     @endif
-    <div class="w-50">
+    <div class="w-25">
         <img class="img-fluid" src="{{ asset('storage/' . $project->image) }}" alt="{{ $project->title }}">
         <p>{{ $project->image_original_name }}</p>
     </div>
     <p><strong>Start date:</strong> {{ Helper::formatDate($project->start_date) }}</p>
-    <p><strong>End date:</strong> {{ isset($project->end_date) ? "$project->end_date" : 'Work in progress' }}</p>
+    <p><strong>End date:</strong>
+        {{ isset($project->end_date) ? Helper::formatDate($project->end_date) : 'Work in progress' }}</p>
     <p><strong>Description:</strong> {!! $project->description !!}</p>
     <a href="{{ route('admin.projects.index') }}" class="btn btn-primary">Return</a>
 @endsection
