daea5bfd63a025398faf7c72b870ff2dd4e721e5
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-03 14:37:30 +0100

    [ add ] title page

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,7 +1,8 @@
 <header class="bg-dark">
-    <nav class="navbar navbar-dark">
-        <div class="container-fluid">
+    <nav class="navbar navbar-dark h-100">
+        <div class="container-fluid d-flex align-items-center">
             <a href="{{ route('home') }}" target="_blank" class="navbar-brand">Vai al sito</a>
+            <h1 class="text-success m-0">Boolfolio</h1>
             <form action="{{ route('logout') }}" method="POST" class="d-flex" role="search">
                 @csrf
                 <button class="btn btn-light" type="submit"><i class="fa-solid fa-right-from-bracket"></i></button>
